30f4b70a49a02928012c5f3d99ab8958bd5c80c8
fabicon2882 <43656659+fabicon2882@users.noreply.github.com>
2021-04-28 19:30:24 -0500

    create form, templates, urls, navbar, scripts

--- crmSystem/urls.py
+++ crmSystem/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from django.urls import path
-
-from leads.views import home_page
+from django.urls import path, include
+from leads.views import landing_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_page)
+    path('', landing_page, name='landing-page'),
+    path('leads/', include('leads.urls', namespace="leads"))
 ]
